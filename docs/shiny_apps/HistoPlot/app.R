397bd0c714301a7435e75cd89a217f2a4a8753b9
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2024-01-05 22:38:01 -0800

    Update app.R

--- docs/shiny_apps/HistoPlot/app.R
+++ docs/shiny_apps/HistoPlot/app.R
@@ -28,5 +28,5 @@ server <- function(input, output) {
 shinyApp(ui = ui, server = server)
 
 path = 'C:/Users/Bruin/Documents/GitHub/UI_STAT251-03_Spring_2024/docs/shiny_apps/HistoPlot/'
-outpath = 'C:/Users/Bruin/Documents/GitHub/UI_STAT251-03_Spring_2024/docs/shiny_htmls/'
+outpath = 'C:/Users/Bruin/Documents/GitHub/UI_STAT251-03_Spring_2024/docs/histogram_viewer/'
 shinylive::export(appdir = path, destdir = outpath)
