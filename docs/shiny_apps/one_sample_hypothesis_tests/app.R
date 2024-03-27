5f691874516b3c72a64c6b1c52d6c26cce535d2d
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2024-03-26 23:57:30 -0700

    updated

--- docs/shiny_apps/one_sample_hypothesis_tests/app.R
+++ docs/shiny_apps/one_sample_hypothesis_tests/app.R
@@ -6,11 +6,318 @@
 #
 #    http://shiny.rstudio.com/
 #
-source('C:/Users/Bruin/Documents/GitHub/UI_STAT251-03_Spring_2024/docs/misc/stat251_tools.R')
+
 library(shiny)
 library(ggplot2)
 library(ggthemes)
 library(latex2exp)
+library(gridExtra)
+library(ggpubr)
+library(plyr)
+library(VennDiagram)
+
+#this function is a helper function for gen.density.plot
+d_limit <- function(x, alpha, test, dist, n) {
+  if(dist == 'z'){
+    y = dnorm(x)
+    q_a = qnorm(1-alpha)
+  }else{
+    y = dt(x, df = n-1)
+    q_a = qt(1-alpha, df = n-1)
+  }
+  
+  
+  if(test == 'two.tailed'){
+    y[which(x>-q_a & x<q_a)] <- NA  
+    return(y)
+  }else if (test == 'upper.tail'){
+    y[which(x < q_a)] <- NA
+    return(y)
+  }else{
+    y[which(x > -q_a)] <- NA
+    return(y)
+  }
+  
+}
+
+
+# this function plots a density curve for the z and t distributions and allows for shading the 
+# the rejection regions on the plot
+gen.density.plot=function(bbox = c(-4,4), n = 10, dist = c('z','t'), alpha = 0.05, obs = 0.5,
+                          test = c('two.tailed','lower.tail', 'upper.tail'), shade = TRUE,
+                          txt.sz = 5){
+  
+  p = ggplot(data.frame(x = bbox), aes(x = x))+
+    theme_bw()
+  if(shade == TRUE){
+    p = p + stat_function(fun = d_limit,
+                          args = list(alpha = alpha,
+                                      test = test,
+                                      dist = dist,
+                                      n = n), 
+                          geom = "area", 
+                          fill = "cyan3", 
+                          alpha = 0.4)
+  }
+  if(dist == 'z'){
+    q_a = qnorm(1-alpha)
+    d_obs = dnorm(obs)
+    d_a = dnorm(q_a)
+    func = dnorm
+    args = list()
+    xlabel = "z"
+  }else{
+    q_a = qt(1-alpha, df = n-1)
+    d_obs = dt(obs, df = n-1)
+    d_a = dt(q_a, df = n-1)
+    func = dt
+    args = list(df = n-1)
+    xlabel = 't'
+  }
+  
+  if(test == 'two.tailed'){
+    xcoords = c(-q_a, q_a, obs)
+    ycoords = c(0,0,0)
+    xendcoords = c(-q_a, q_a, obs)
+    yendcoords = c(d_a, d_a, d_obs)
+    if(dist == 'z'){
+      lbl1 = c(TeX("$z_{\\alpha/2}$"),TeX("$z_{1-\\alpha/2}$"),TeX("$z_{obs}$"))
+    }else{
+      lbl1 = c(TeX("$t_{\\alpha/2}$"),TeX("$t_{1-\\alpha/2}$"),TeX("$t_{obs}$"))
+    }
+  }else if (test == 'upper.tail'){
+    xcoords = c(q_a, obs)
+    ycoords = c(0,0)
+    xendcoords = c(q_a, obs)
+    yendcoords = c(d_a, d_obs)
+    if(dist == 'z'){
+      lbl1 = c(TeX("$z_{1-\\alpha}$"),TeX("$z_{obs}$"))
+    }else{
+      lbl1 = c(TeX("$t_{1-\\alpha}$"),TeX("$t_{obs}$"))
+    }
+  }else{
+    xcoords = c(-q_a, obs)
+    ycoords = c(0,0)
+    xendcoords = c(-q_a, obs)
+    yendcoords = c(d_a, d_obs)
+    if(dist == 'z'){
+      lbl1 = c(TeX("$z_{\\alpha}$"), TeX("$z_{obs}$"))
+    }else{
+      lbl1 = c(TeX("$t_{\\alpha}$") ,TeX("$t_{obs}$"))
+    }
+  }
+  
+  if(test == 'two.tailed'){
+    final = p+stat_function(fun = func,
+                            args = args,
+                            size = 1)+
+      geom_segment(aes(x=xcoords[1:2],
+                       y=ycoords[1:2],
+                       xend=xendcoords[1:2],
+                       yend=yendcoords[1:2]),
+                   size = 0.9,
+                   linetype = 'dashed')+
+      geom_segment(aes(x=xcoords[3],
+                       y=ycoords[3],
+                       xend=xendcoords[3],
+                       yend=yendcoords[3]),
+                   size = 0.9,
+                   linetype = 'dashed')+
+      geom_point(aes(x = xcoords[1:2], y = ycoords[1:2]), size = 3)+
+      geom_label(aes(x = xcoords[1:2], y = ycoords[1:2]-0.025), label = lbl1[1:2], size = txt.sz)+
+      geom_point(aes(x = xcoords[3], y = ycoords[3]), size = 3)+
+      geom_label(aes(x = xcoords[3], y = ycoords[3]-0.025), label = lbl1[3], size = txt.sz)+
+      geom_hline(yintercept = 0, size = 1)+
+      xlab(xlabel)+
+      ylab('Probability Density')+
+      ggtitle(TeX('Distribution under $H_0$'))+
+      theme(axis.title.x = element_text(size = 14),
+            axis.title.y = element_text(size = 14))
+  }else{
+    final = p+stat_function(fun = func,
+                            args = args,
+                            size = 1)+
+      geom_segment(aes(x=xcoords,
+                       y=ycoords,
+                       xend=xendcoords,
+                       yend=yendcoords), 
+                   size = rep(0.9, length(xcoords)), 
+                   linetype = rep('dashed',length(xcoords)))+
+      geom_point(aes(x = xcoords, y = ycoords), size = 3)+
+      geom_label(aes(x = xcoords, y = ycoords-0.025), label = lbl1, size = txt.sz)+
+      geom_hline(yintercept = 0, size = 1)+
+      xlab(xlabel)+
+      ylab('Probability Density')+
+      ggtitle(TeX('Distribution under $H_0$'))+
+      theme(axis.title.x = element_text(size = 12),
+            axis.title.y = element_text(size = 12))
+  }
+  
+  
+  return(final)
+}
+
+
+
+
+
+
+
+
+# standard error for a sample proportion 
+one.sample.prop.SE = function(phat, n){
+  SE = sqrt((phat*(1-phat))/n)
+  return(SE)
+}
+
+
+# confidence interval for a population proportion 
+one.sample.prop.CI = function(phat, n, alpha = 0.05, verbose = FALSE){
+  SE = one.sample.prop.SE(phat, n)
+  standard.score = qnorm(1-(alpha/2))
+  MOE = standard.score*SE
+  lower.bound = phat-MOE
+  upper.bound = phat+MOE
+  if(isTRUE(verbose)){
+    print(noquote(paste0(rep('-', 50), collapse = '')))
+    print(noquote(paste0('Estimate = ', round(phat, 4))))
+    print(noquote(paste0('Critical Value  = ', round(standard.score, 4))))
+    print(noquote(paste0('Estimated Standard Error = ', round(SE, 4))))
+    print(noquote(paste0('Margin of error = ', round(MOE, 4))))
+    print(noquote(paste0((1-alpha)*100, ' % CI = ', paste0('[', round(max(lower.bound,0),4), ',',
+                                                           round(min(upper.bound, 1), 4), ']', 
+                                                           collapse = ''))))
+    print(noquote(paste0(rep('-', 50), collapse = '')))
+  }
+  return(c(lower.bound, upper.bound))
+}
+
+
+
+# test for a population proportion 
+one.sample.prop.test = function(p0, phat, n, alpha = 0.05, test = c('lower.tail','upper.tail','two.tail'),
+                                verbose = TRUE){
+  
+  estimate.SE = one.sample.prop.SE(phat, n)
+  Zobs = (phat - p0)/estimate.SE
+  if(test == 'two.tail'){
+    critical.value = qnorm((1-(alpha/2)))
+    alt.hyp = 'p != '
+    pvalue = 2*(1-pnorm(abs(Zobs)))
+  }else if(test == 'lower.tail'){
+    critical.value = qnorm(alpha)
+    alt.hyp = 'p < '
+    pvalue = pnorm(Zobs)
+  }else{
+    critical.value = qnorm(1-alpha)
+    alt.hyp = 'p > '
+    pvalue = 1-pnorm(Zobs)
+  }
+  
+  CI = one.sample.prop.CI(phat, n, alpha)
+  decision = ifelse(pvalue<alpha, 'reject H0', 'fail to reject H0')
+  if(isTRUE(verbose)){
+    print(noquote(paste0(paste0(rep('=', 20), collapse = ''), ' test results ', paste0(rep('=', 20), collapse = ''))))
+    print(noquote(paste0('test type = ', test)))
+    print(noquote(paste0('H0: p0 = ', p0)))
+    print(noquote(paste0('HA: ', alt.hyp, p0)))
+    print(noquote(paste0('estimate = ', round(phat,4))))
+    print(noquote(paste0('Estimated Standard Error = ', round(estimate.SE,4))))
+    print(noquote(paste0('Critical Value = ', round(critical.value, 4))))
+    print(noquote(paste0((1-alpha)*100, '% CI = ', paste0('[', max(round(CI[1],4), 0),',',
+                                                          round(CI[2],4),']', 
+                                                          collapse = ''))))
+    print(noquote(paste0('Test statistic = ', round(Zobs, 4))))
+    print(noquote(paste0('Pvalue = ', round(pvalue, 4))))
+    print(noquote(paste0('Decision: ', decision)))
+    print(noquote(paste0(rep("=", 54), collapse = '')))
+  }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+#standard error for a sample mean 
+one.sample.t.SE = function(s, n){
+  SE = s/sqrt(n)
+  return(SE)
+}
+
+
+#confidence interval for a population mean 
+one.sample.t.CI = function(xbar, s, n, alpha = 0.05, verbose = FALSE){
+  SE = one.sample.t.SE(s, n)
+  t.score = qt(1-(alpha/2), df = n-1)
+  MOE = t.score*SE
+  lower.bound = xbar-MOE
+  upper.bound = xbar+MOE
+  if(isTRUE(verbose)){
+    print(noquote(paste0(rep('-', 50), collapse = '')))
+    print(noquote(paste0('Estimate = ', round(xbar, 4))))
+    print(noquote(paste0('Critical Value  = ', round(t.score, 4))))
+    print(noquote(paste0('Estimated Standard Error = ', round(SE, 4))))
+    print(noquote(paste0('Margin of error = ', round(MOE, 4))))
+    print(noquote(paste0((1-alpha)*100, ' % CI = ', paste0('[', round(lower.bound,4),',', 
+                                                           round(upper.bound,4), ']',
+                                                           collapse = ''))))
+    print(noquote(paste0(rep('-', 50), collapse = '')))
+  }
+  return(c(lower.bound, upper.bound))
+}
+
+
+# one sample t test 
+one.sample.t.test = function(m0, xbar, s, n, alpha = 0.05, test = c('lower.tail','upper.tail','two.tail'),
+                             verbose = TRUE){
+  
+  df = n - 1
+  estimate.SE = one.sample.t.SE(s, n)
+  tobs = (xbar - m0)/estimate.SE
+  if(test == 'two.tail'){
+    critical.value = qt((1-(alpha/2)), df = df)
+    alt.hyp = 'm != '
+    pvalue = 2*(1-pt(abs(tobs), df = df))
+  }else if(test == 'lower.tail'){
+    critical.value = qt(alpha, df = df)
+    alt.hyp = 'm < '
+    pvalue = pt(tobs, df = df)
+  }else{
+    critical.value = qt(1-alpha, df = df)
+    alt.hyp = 'm > '
+    pvalue = 1-pt(tobs, df = df)
+  }
+  
+  CI = one.sample.t.CI(xbar, s, n, alpha)
+  decision = ifelse(pvalue<alpha, 'reject H0', 'fail to reject H0')
+  if(isTRUE(verbose)){
+    print(noquote(paste0(paste0(rep('=', 20), collapse = ''), ' test results ', paste0(rep('=', 20), collapse = ''))))
+    print(noquote(paste0('test type = ', test)))
+    print(noquote(paste0('H0: m0 = ', m0)))
+    print(noquote(paste0('HA: ', alt.hyp, m0)))
+    print(noquote(paste0('Estimate = ', round(xbar,4))))
+    print(noquote(paste0('Estimated Standard Error = ', round(estimate.SE,4))))
+    print(noquote(paste0('Critical Value = ', round(critical.value, 4))))
+    print(noquote(paste0((1-alpha)*100, '% CI = ', paste0('[', max(round(CI[1],4), 0),',',
+                                                          round(CI[2],4),']', 
+                                                          collapse = ''))))
+    print(noquote(paste0('Test statistic = ', round(tobs, 4))))
+    print(noquote(paste0('Degrees of Freedom = ', round(df, 4))))
+    print(noquote(paste0('Pvalue = ', round(pvalue, 4))))
+    print(noquote(paste0('Decision: ', decision)))
+    print(noquote(paste0(rep("=", 54), collapse = '')))
+  }
+} 
+
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
